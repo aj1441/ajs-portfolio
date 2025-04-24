517525f09dac45bb43ba0d2985300085e7ab9f01
aj1441 <167340937+aj1441@users.noreply.github.com>
2025-04-24 13:35:35 -0700

    corrected file paths for img, adjusted margin on skillIconCategory

--- src/data/logos.jsx
+++ src/data/logos.jsx
@@ -1,138 +1,138 @@
 const skills = [
     {
       title: "JavaScript",
-      image: "public/img/javascript.svg",
+      image: "/img/javascript.svg",
       alt: "JavaScript logo"
     },
     {
       title: "React",
-      image: "public/img/react.svg",
+      image: "/img/react.svg",
       alt: "React logo"
     },
     {
       title: "Node.js",
-      image: "public/img/nodejs.svg",
+      image: "/img/nodejs.svg",
       alt: "Node.js logo"
     },
     {
       title: "HTML5",
-      image: "public/img/html-5.svg",
+      image: "/img/html-5.svg",
       alt: "HTML5 logo"
     },
     {
       title: "CSS3",
-      image: "public/img/css-3.svg",
+      image: "/img/css-3.svg",
       alt: "CSS3 logo"
     },
     {
       title: "PostgreSQL",
-      image: "public/img/postgresql.svg",
+      image: "/img/postgresql.svg",
       alt: "PostgreSQL logo"
     },
     {
       title: "Vite",
-      image: "public/img/vite.svg",
+      image: "/img/vite.svg",
       alt: "Vite logo"
     },
     {
       title: "Git",
-      image: "public/img/git.svg",
+      image: "/img/git.svg",
       alt: "Git logo"
     },
     {
       title: "GitHub",
-      image: "public/img/github-mark-white.svg",
+      image: "/img/github-mark-white.svg",
       alt: "GitHub logo"
     }, 
     {
       title: "Express.JS",
-      image: "public/img/Express.svg",
+      image: "/img/express.png",
       alt: "Express JS logo"
     }, 
     {
       title: "VS Code",
-      image: "public/img/VS-Code.svg",
+      image: "/img/VS-Code.svg",
       alt: "VS Code logo"
     },
     {
       title: "Airtable",
-      image: "public/img/airtable.svg",
+      image: "/img/airtable.svg",
       alt: "Airtable logo"
     },
     {
       title: "Chakra UI",
-      image: "public/img/chakra.svg",
+      image: "/img/chakra.svg",
       alt: "Chakra UI logo"
     },
     {
       title: "Python",
-      image: "public/img/python.svg",
+      image: "/img/python.svg",
       alt: "Python logo"
     },
     {
       title: "Firebase SDK",
-      image: "public/img/firebase.svg",
+      image: "/img/firebase.svg",
       alt: "Firebase logo"
     },
     {
       title: "Figma",
-      image: "public/img/figma.svg",
+      image: "/img/figma.svg",
       alt: "Figma logo"
     },
     {
       title: "WordPress",
-      image: "public/img/wordpress.svg",
+      image: "/img/wordpress.svg",
       color: "00749C",
       alt: "WordPress logo"
     },
     {
       title: "Heroku",
-      image: "public/img/heroku.svg",
+      image: "/img/heroku.svg",
       alt: "Heroku logo"
     },
     {
       title: "Jira",
-      image: "public/img/jira.svg",
+      image: "/img/jira.svg",
       alt: "Jira logo"
     },
     {
       title: "JSON",
-      image: "public/img/JSON.svg",
+      image: "/img/JSON.svg",
       alt: "JSON logo"
     },
     {
       title: "Postman",
-      image: "public/img/postman.svg",
+      image: "/img/postman.svg",
       alt: "Postman logo"
         },
     {
       title: "Powershell",
-      image: "public/img/Powershell.svg",
+      image: "/img/Powershell.svg",
       alt: "Powershell logo"
         },
     {
       title: "Render",
-      image: "public/img/Render.png",
+      image: "/img/Render.png",
       alt: "Render logo"
         },
     {
       title: "Rest API",
-      image: "public/img/rest-api.svg",
+      image: "/img/rest-api.svg",
       alt: "Rest API logo"
         },
     {
       title: "Trello",
-      image: "public/img/Trello.svg",
+      image: "/img/Trello.svg",
       alt: "Trello logo"
         },
     {
       title: "Slack",
-      image: "public/img/Slack.svg",
+      image: "/img/Slack.svg",
       alt: "Slack logo"
         },
     {
       title: "Canva",
-      image: "public/img/Canva.svg",
+      image: "/img/Canva.svg",
       alt: "Canva logo"
     }
 ];
